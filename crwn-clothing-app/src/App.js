1d20eadc16da59390083a00c4326baec1a498e86
unknown <devethancraig@gmail.com>
2019-12-08 12:55:05 -0700

    CollectionPreview Component && CollectionItem Component Added to Application - Small Bug Fixes As Well

--- crwn-clothing-app/src/App.js
+++ crwn-clothing-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import { Route, Switch } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage.component';
-import { ShopPage } from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
