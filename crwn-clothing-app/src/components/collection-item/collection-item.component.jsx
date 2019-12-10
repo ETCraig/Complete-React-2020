cd8c78d6249419efe24f4c64c067cc468fa42f80
unknown <devethancraig@gmail.com>
2019-12-09 20:56:40 -0700

    Users Can Now Add Single Item Into Their Cart

--- crwn-clothing-app/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing-app/src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './collection-item.styles.scss';
 
+import { addItem } from '../../redux/cart/cart.actions';
 import CustomButton from '../custom-button/custom-button.component';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-    <div className="collection-item">
-        <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-        <div className="collection-footer">
-            <span className="name">{name}</span>
-            <span className="price">{price}</span>
-        </div>
-        <CustomButton inverted className="custom-button"> Add To Cart </CustomButton>
-    </div>
-);
+import { connect } from 'react-redux';
 
-export default CollectionItem;
+const CollectionItem = ({ item, addItem }) => {
+    const { id, name, price, imageUrl } = item;
+    return (
+        <div className="collection-item">
+            <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+            <div className="collection-footer">
+                <span className="name">{name}</span>
+                <span className="price">{price}</span>
+            </div>
+            <CustomButton inverted onClick={() => addItem(item)}> Add To Cart </CustomButton>
+        </div>
+    );
+}
+
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
