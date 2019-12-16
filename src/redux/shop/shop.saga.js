d18d4c97547c64074fb45d3ba1de89d2b4300a6b
unknown <devethancraig@gmail.com>
2019-12-15 23:36:11 -0700

    Transitioned Async Colls. Fetch From thunk To Saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,9 +1,26 @@
-import { takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
+import { convertCollectionsSnapShotToMap, firestore } from '../../firebase/firebasae.utils';
+import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop.actions';
 import ShopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
+    try {
+        const collectionRef = firestore.collection('collections');
 
+        const snapShot = yield collectionRef.get();
+
+        const collectionsMap = yield call(convertCollectionsSnapShotToMap, snapShot);
+
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message));
+    }
+
+    // collectionRef.get().then(snapshot => {
+    //     const collectionsMap = convertCollectionsSnapShotToMap(snapshot);
+    //     dispatch(fetchCollectionsSuccess(collectionsMap));
+    // }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {
