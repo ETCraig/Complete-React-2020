f36e27e170e0ba3584ca2b67ff666b6ce20d5a25
unknown <devethancraig@gmail.com>
2019-12-17 13:22:59 -0700

    Created '/payment' Route In The server.js File - Installed && Used axios To Created Front End Function To Send Payment Request To NodeJS '/payment' Route

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const path = require('path');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
-const port = process.env.PORT || 5000;
+const Port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,4 +26,20 @@ if (process.env.NODE_ENV === 'production') {
 app.listen(Port, error => {
     if (error) throw error;
     console.log(`Server Running on Port: ${Port}.`);
+});
+
+app.post('/payment', (req, res) => {
+    const body = {
+        source: req.body.token.id,
+        amount: req.body.amount,
+        currency: 'usd'
+    }
+
+    stripe.charges.create(body, (error, response) => {
+        if (error) {
+            res.status(500).send({ error: error });
+        } else {
+            res.status(200).send({ success: response });
+        }
+    });
 });
